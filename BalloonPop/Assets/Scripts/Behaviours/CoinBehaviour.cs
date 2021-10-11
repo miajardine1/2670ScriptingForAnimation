30f6b17941169f186a8569a3c4241c342782da7c
Miriam Jardine <miajardine1@gmail.com>
2021-10-11 12:08:56 -0600

    Target Behaviour

---
+++ BalloonPop/Assets/Scripts/Behaviours/CoinBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    public IntData intDateObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        intDataObj.value++;
+    }
+//rewritten from https://github.com/anthonyromrell/2670ScriptingFall2021/tree/main/2670/Assets/Scripts//
+}
---
+++ BalloonPop/Assets/Scripts/Behaviours/SimpleTextBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class SimpleTextBehaviour : MonoBehaviour
+{
+    private Text Label;
+
+    private void Awake()
+    {
+        Label = GetComponent<Text>();
+    }
+
+    public void UpdateTextValue(IntData obj)
+    {
+        Label.text = obj.value.ToString();
+    }
+    
+    //rewritten from https://github.com/anthonyromrell/2670ScriptingFall2021/tree/main/2670/Assets/Scripts// 
+}
